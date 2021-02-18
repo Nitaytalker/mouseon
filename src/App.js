ac03acd50e1b1a11848ee131b47ac7fff5e5d584
Nitaytalker <nitay68428@gmail.com>
2021-02-18 12:23:07 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MouseText from "./mousetext"
+
+function App() {
+  return (
+    <div className="App">
+      <MouseText/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
